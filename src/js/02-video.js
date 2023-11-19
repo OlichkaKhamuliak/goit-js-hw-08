c2a6c5eb809c2ea53eb1c5daab0d8b8e6dd21a5b
OlichkaKhamuliak <olkoboyko97@gmail.com>
2023-11-19 13:09:05 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const TIME_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('#vimeo-player');
+
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(handleTimeUpdate, 1000));
+
+function handleTimeUpdate(evt) {
+    localStorage.setItem(TIME_KEY, evt.seconds);
+};
+
+const savedTime = JSON.parse(localStorage.getItem(TIME_KEY));
+
+if (savedTime) {
+    player.setCurrentTime(savedTime);
+}
